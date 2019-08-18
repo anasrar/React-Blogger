8216b5d16e97a95d8197f81a20239c27caf7e7b3
Anas RAR <mynameanasrar@gmail.com>
2019-08-18 23:55:06 +0700

    build

---
+++ precache-manifest.4a4342094db624d174644fbec88a8027.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe1fd699b8f2004db89b8b33f928633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e1671e5204bdd241c1",
+    "url": "/static/js/2.9fd02736.chunk.js"
+  },
+  {
+    "revision": "108157c3e078bff76f9b",
+    "url": "/static/js/main.f5b3cf2b.chunk.js"
+  },
+  {
+    "revision": "b52db2c25d7ed6eb68e3",
+    "url": "/static/js/runtime~main.a8c1b21d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b9a45de4bc4cc5397a71eb1cacffe23.js"
+  "/precache-manifest.4a4342094db624d174644fbec88a8027.js"
 );
 
 self.addEventListener('message', (event) => {
